5c5d9d3cf427dee0009e0302b63d1759935ec4e4
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2025-02-11 13:44:32 +0300

    feat: add parsing dota2 api

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+var players = map[string]string{
+	"Danek": "412196175",
+}
 
 func main() {
-	fmt.Println("Boot")
+	recentMatches("Danek")
+	showInfoOfMatchByPlayer(players["Danek"], "8093496511")
+}
+
+func recentMatches(name string) {
+	resp, err := http.Get("https://api.opendota.com/api/players/" + players[name] + "/matches/?date=18")
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	fmt.Println(body)
+	// var matches []PlayerMatch
+	// err = json.Unmarshal(body, &matches)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	// for _, v := range matches {
+	// 	fmt.Println(v.toString())
+	// }
+
+}
+
+func showInfoOfMatchByPlayer(playerId string, matchId string) {
+	fmt.Println(playerId)
+	resp, err := http.Get("https://api.opendota.com/api/matches/" + matchId)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	fmt.Println(body)
+	// var match Match
+	// err = json.Unmarshal(body, &match)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// fmt.Println(match)
 }
